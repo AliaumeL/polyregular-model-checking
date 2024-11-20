9fb979d46107a69e1e5bf81f9e740ab012099d6a
Aliaume LOPEZ <lopezaliaume@gmail.com>
2024-11-20 14:46:53 +0100

    testing MONA

--- polyreg/src/SmtSolver.hs
+++ polyreg/src/SmtSolver.hs
@@ -3,6 +3,8 @@ module SmtSolver where
 import TwoSortedFormulas (Formula(..), Quant(..), Sort(..))
 import QuantifierFree
 
+import Data.List (isInfixOf)
+
 import System.Process
 
 intersperse :: a -> [a] -> [a]
@@ -31,20 +33,20 @@ toMona (FBin Disj left right) = "( " ++ toMona left ++ " | " ++ toMona right ++ 
 toMona (FBin Impl left right) = "( " ++ toMona left ++ " => " ++ toMona right ++ " )"
 toMona (FBin Equiv left right) = "( " ++ toMona left ++ " <=> " ++ toMona right ++ " )"
 toMona (FNot inner) = "~( " ++ toMona inner ++ " )"
-toMona (FTestPos Eq x y) = x ++ " = " ++ y
-toMona (FTestPos Neq x y) = x ++ " != " ++ y
-toMona (FTestPos Lt x y) = x ++ " < " ++ y
-toMona (FTestPos Le x y) = x ++ " <= " ++ y
-toMona (FTestPos Gt x y) = x ++ " > " ++ y
-toMona (FTestPos Ge x y) = x ++ " >= " ++ y
-toMona (FTag x tag) = x ++ " in T" ++ show tag
-toMona (FLetter x letter) = x ++ " in L" ++ show letter
+toMona (FTestPos Eq x y)  = "(" ++ x ++ " = " ++ y ++ ")"
+toMona (FTestPos Neq x y) = "(" ++ x ++ " != " ++ y ++ ")"
+toMona (FTestPos Lt x y)  = "(" ++ x ++ " < " ++ y ++ ")"
+toMona (FTestPos Le x y)  = "(" ++ x ++ " <= " ++ y ++ ")"
+toMona (FTestPos Gt x y)  = "(" ++ x ++ " > " ++ y ++ ")"
+toMona (FTestPos Ge x y)  = "(" ++ x ++ " >= " ++ y ++ ")"
+toMona (FTag x tag) = "(" ++ x ++ " in T" ++ tag ++ ")"
+toMona (FLetter x letter) = "(" ++ x ++ " in L" ++ [letter] ++ ")"
 
-encodeMona :: [String] -> [String] -> Formula String Char -> String
-encodeMona alphabet tags formula = unlines $ [preamble, alphabetVarsDef, tagsVarsDef, wordVars, labelsAt, labelsUnion, wordUnion, disjoint, covering, formula']
+encodeMona :: String -> [String] -> Formula String Char -> String
+encodeMona alphabet tags formula = unlines $ [preamble, alphabetVarsDef, tagsVarsDef, wordVars, labelsAt, labelsUnion, wordUnion, disjoint, wordDisjoint, covering, formula']
     where
         preamble = "m2l-str;"
-        alphabetVars = ["L" ++ name | name <- alphabet]
+        alphabetVars = ["L" ++ [name] | name <- alphabet]
         tagsVars     = ["T" ++ name | name <- tags]
         wordVars = "var2 W,T;"
 
@@ -52,13 +54,14 @@ encodeMona alphabet tags formula = unlines $ [preamble, alphabetVarsDef, tagsVar
         tagsVarsDef = "var2 " ++ unwords (intersperse "," tagsVars) ++ ";"
 
         labelAtI :: Int -> String -> String
-        labelAtI i name = "assert (" ++ name ++ " = {" ++ show i ++ "};"
+        labelAtI i name = "assert (" ++ name ++ " = {" ++ show i ++ "});"
 
         labelsAt :: String
-        labelsAt = concat [labelAtI i name | (i, name) <- zip [0..] tagsVars ]
+        labelsAt = concat . intersperse "\n" $ [labelAtI i name | (i, name) <- zip [0..] tagsVars ]
 
         labelsUnion = "assert (T = " ++ unwords (intersperse " union " tagsVars) ++ ");"
         wordUnion = "assert (W = " ++ unwords (intersperse " union " alphabetVars) ++ ");"
+        wordDisjoint = unlines [ "assert (L" ++ [a] ++ " inter L" ++ [b] ++ " = empty);" | a <- alphabet, b <- alphabet, a /= b ]
         disjoint = "assert (T inter W = empty);"
 
         covering = "assert (all1 x: (x in T) | (x in W));"
@@ -66,11 +69,11 @@ encodeMona alphabet tags formula = unlines $ [preamble, alphabetVarsDef, tagsVar
         formula' = toMona formula ++ ";"
 
 parseMonaOutput :: String -> SmtResult
-parseMonaOutput output = if "Formula is valid" `elem` lines output then Sat else if "Formula is unsatisfiable" `elem` lines output then Unsat else Unknown
+parseMonaOutput output = if "Formula is valid" `isInfixOf` output then Sat else if "A satisfying example" `isInfixOf` output then Sat else if "Formula is unsatisfiable" `elem` lines output then Unsat else Unknown
 
 -- use TmpDir to store the file
 runMona :: String -> IO SmtResult
 runMona input = do
     writeFile "tmp.mona" input
-    output <- readProcess "mona" ["tmp.mona"] ""
+    output <- readProcess "mona" ["-q", "tmp.mona"] ""
     return $ parseMonaOutput output
