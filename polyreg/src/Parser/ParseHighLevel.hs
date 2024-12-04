78a9f3b9a4207104237dd5c3e581e7dd2aaa4bed
Rafał Stefański <rafal.stefanski@mimuw.edu.pl>
2024-12-04 10:12:52 +0100

    Had some spare time, so I decorated parsed program with some types.

--- polyreg/src/Parser/ParseHighLevel.hs
+++ polyreg/src/Parser/ParseHighLevel.hs
@@ -1,10 +1,10 @@
 module Parser.ParseHighLevel where
 
 import Parser.HighLevelForProgram.Par   ( pProgram, myLexer )
-import Parser.ParsedToForProgram ( parsedToForProgram )
+import Parser.ParsedToForProgram ( parsedToForProgram, PAProgram )
 import ForPrograms
 
-parseHighLevel :: String -> Either String UProgram
+parseHighLevel :: String -> Either String PAProgram
 parseHighLevel s = do
     case pProgram (myLexer s) of
         Left err -> Left $ show err
