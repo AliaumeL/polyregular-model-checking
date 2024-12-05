a3750503c3318dd3ab6bb0e510dd39f005a35c12
Aliaume LOPEZ <ad.lopez@uw.edu.pl>
2024-12-05 16:17:10 +0100

    adding bibtex example

--- polyreg/test/ReductionLitEqSpec.hs
+++ polyreg/test/ReductionLitEqSpec.hs
@@ -17,9 +17,23 @@ untypeProgram = fmap (const ())
 
 spec :: Spec
 spec = do 
-    testProgram <- runIO $ parseFromFile "assets/litteral_test.pr"
-    let infered = fromRight' (inferProgram (fromRight' testProgram))
-    describe "We actually remove literal tests calls" $ do
+    describe "We actually remove literal tests calls on `litteral_test`" $ do
+        testProgram <- runIO $ parseFromFile "assets/litteral_test.pr"
+        let infered = fromRight' (inferProgram (fromRight' testProgram))
+        it "Starts with a program with some BLitEq calls" $ do 
+            (hasLitEq infered) `shouldBe` True
+        it "Ends with a program without function calls" $ do
+            let eliminated = removeBLitEq infered
+            (hasLitEq eliminated) `shouldBe` False
+        it "The program still works" $ do
+            let eliminated = removeBLitEq infered
+            let input = "go to park"
+            let expected = runProgramString (untypeProgram infered)    input
+            let actual = runProgramString   (untypeProgram eliminated) input
+            actual `shouldBe` expected
+    describe "We actually remove literal tests calls on `bibtex`" $ do
+        testProgram <- runIO $ parseFromFile "assets/bibtex.pr"
+        let infered = fromRight' (inferProgram (fromRight' testProgram))
         it "Starts with a program with some BLitEq calls" $ do 
             (hasLitEq infered) `shouldBe` True
         it "Ends with a program without function calls" $ do
